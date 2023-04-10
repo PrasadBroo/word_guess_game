d0ed7cc5e6b3c379cd73276750b8e10bbe1e519c
Prasad Shinde <sketchware1@gmail.com>
2023-04-10 13:59:31 +0530

    added loader

--- src/pages/JoinRandomRoompage.tsx
+++ src/pages/JoinRandomRoompage.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { socket } from "../services/socket";
 import { UserContext } from "../contexts/userContext";
 import { useNavigate } from "react-router-dom";
+import { PacmanLoader } from "react-spinners";
 
 export default function JoinRandomRoompage() {
   const { currentUser } = useContext(UserContext);
@@ -15,9 +16,8 @@ export default function JoinRandomRoompage() {
   return (
     <div className="joinanyroom">
       <div className=" transition dark:text-white dark:bg-secondary text-2xl w-full flex items-center justify-center">
-        Wating for user  <span className="mx-2 animate-bounce ">.</span>{" "}
-        <span className="mr-2 animate-bounce  ">.</span>{" "}
-        <span className=" animate-bounce  ">.</span>
+        {/* <span className="mr-4"> Wating for user </span> */}
+        <PacmanLoader color="#fff" size={25} speedMultiplier={0.6} />
       </div>
     </div>
   );
