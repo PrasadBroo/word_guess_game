2878f0b4724a2621ae59a39e325bf0df2adf95bc
Prasad Shinde <sketchware1@gmail.com>
2023-04-15 13:18:18 +0530

    changed loading screen

--- src/pages/JoinRandomRoompage.tsx
+++ src/pages/JoinRandomRoompage.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react";
 import { socket } from "../services/socket";
 import { UserContext } from "../contexts/userContext";
 import { useNavigate } from "react-router-dom";
-import { PacmanLoader } from "react-spinners";
+import { BarLoader, PacmanLoader } from "react-spinners";
 
 export default function JoinRandomRoompage() {
   const { currentUser } = useContext(UserContext);
@@ -15,9 +15,9 @@ export default function JoinRandomRoompage() {
 
   return (
     <div className="joinanyroom">
-      <div className=" transition dark:text-white dark:bg-secondary text-2xl w-full flex items-center justify-center">
-        {/* <span className="mr-4"> Wating for user </span> */}
-        <PacmanLoader color="#fff" size={25} speedMultiplier={0.6} />
+      <div className=" transition dark:text-white dark:bg-secondary text-2xl w-full ">
+        <span className="mr-4 block"> Searching for opponent </span>
+        <BarLoader color="#fff" className="mx-auto mt-4" />
       </div>
     </div>
   );
