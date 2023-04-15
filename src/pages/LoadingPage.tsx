2afbad185835c63a049c35d297159239c12c6077
Prasad Shinde <sketchware1@gmail.com>
2023-04-15 13:28:26 +0530

    bugs fixes

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,8 +1,10 @@
+import { BarLoader } from "react-spinners";
 
 export default function LoadingPage() {
   return (
     <div className=" transition dark:text-white dark:bg-secondary text-2xl w-full flex items-center justify-center">
-      Wating for user 1/2
+      <span className="mr-4 block"> Searching for opponent </span>
+      <BarLoader color="#fff" className="mx-auto mt-4" />
     </div>
   );
 }
